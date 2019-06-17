62b94c4f830429b0bfdb6a31d16f49c08659412d
lilycoco <ry.sep27@gmail.com>
2019-06-17 22:22:42 +0900

    add new course from eggHead

--- start-react/src/App.js
+++ start-react/src/App.js
@@ -1,29 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useContext, useReducer, useEffect} from 'react'
+import PropTypes from 'prop-types'
+import * as GitHub from '../github-client'
 
+function Query({query, variables, normalize = data => data, children}) {
+  const client = useContext(GitHub.Context)
+  const [state, setState] = useReducer(
+    (state, newState) => ({...state, ...newState}),
+    {
+      loaded: false,
+      fetching: false,
+      data: null,
+      error: null,
+    },
+  )
 
-function App() {
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(
+    () => {
+      setState({fetching: true})
+      client
+        .request(query, variables)
+        .then(res =>
+          setState({
+            data: normalize(res),
+            error: null,
+            loaded: true,
+            fetching: false,
+          }),
+        )
+        .catch(error =>
+          setState({
+            error,
+            data: null,
+            loaded: false,
+            fetching: false,
+          }),
+        )
+    },
+    [query, variables],
+  )
+
+  return children(state)
 }
 
+Query.propTypes = {
+  query: PropTypes.string.isRequired,
+  variables: PropTypes.object,
+  children: PropTypes.func.isRequired,
+  normalize: PropTypes.func,
+}
 
-export default App;
+export default Query
